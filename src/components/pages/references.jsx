6b377f968b45684099aa64bd2033d5fdd638f46b
REWraith <wraithre@outlook.com>
2024-11-02 17:58:32 +0300

    Fotoğraf Güncellemesi

--- src/components/pages/references.jsx
+++ src/components/pages/references.jsx
@@ -7,7 +7,7 @@ const references = [
     { city: 'Ankara', country: 'Ankara', description: 'Ankara Etlik Entegre Sağlık Kampüsü', image: 'https://turkerler.com/media/4-240918115350.jpg' },
     { city: 'Yozgat', country: 'Yozgat', description: 'Yozgat Eğitim ve Araştırma Hastanesi', image: 'https://ronesans.com/content/files/yozgat-seh-06632632db20b7a23d4.45889694.webp' },
     { city: 'Adana', country: 'Adana', description: 'Adana Entegre Sağlık Kampüsü', image: 'https://www.pppadanahastanesi.com/upload/Node/21082/xpics/001.p1.jpg' },
-    { city: 'Elazığ', country: 'Elazığ', description: 'Elazığ Entegre Sağlık Kampüsü', image: 'https://lh3.googleusercontent.com/proxy/lQVv6YMzeE4IENaSDuLDf7raODVCg6RwfIR1Ze1uJ-Mf41q796U8ttTQse_AdZhvL00vKzD-Um_54VvMElIgk-VHS8loVLc4raL1Xj4P8-5gJxCj7JlUgGOVGZqpAYQAaVj0LDuC9cosLjtlyWsCl1hG0Cw_N04-d0HNddi9NEMHxLcW72dk' },
+    { city: 'Elazığ', country: 'Elazığ', description: 'Elazığ Entegre Sağlık Kampüsü', image: 'https://i.imgur.com/lGswA6W.jpeg' },
     { city: 'Ankara', country: 'Ankara', description: 'Türkiye Barolar Birliği Hizmet Binası ve Sosyal Tesisleri', image: 'https://avatars.mds.yandex.net/get-altay/1992662/2a0000016fd217f49933ebc40a3a457d3962/orig' },
     { city: 'Ankara', country: 'Ankara', description: 'T.B.M.M. Alüminyum Doğrama ve Yenileme', image: 'https://i.imgur.com/5tphzCm.png' },
     { city: 'Ankara', country: 'Ankara', description: 'Elvankent Gimsa', image: 'https://safagrupinsaat.com/wp-content/uploads/2017/03/IMG_6898.jpg' },
