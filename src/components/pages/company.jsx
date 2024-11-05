4adfb6f31be30ef48fcbc85c85a2c5819e1dcacc
REWraith <wraithre@outlook.com>
2024-11-05 17:19:45 +0300

    Şirket kısmı düzenleme

--- src/components/pages/company.jsx
+++ src/components/pages/company.jsx
@@ -13,11 +13,11 @@ function HeroSection() {
         </h1>
         <p className="text-lg md:text-xl mb-6">
         Pusula Alüminyum, alüminyum sektöründe lider konumda bulunan, yenilikçi ve müşteri odaklı bir şirkettir. Yılların tecrübesi ve uzman kadrosu ile yüksek kaliteli alüminyum ürünleri üretmekte ve geniş bir yelpazede hizmet sunmaktadır. Misyonumuz, sektörde en iyi çözümleri sağlayarak müşteri memnuniyetini en üst düzeyde tutmak ve sürdürülebilir başarı elde etmektir. <br /><br />
-          <p className='text-orange-500'><strong>Misyonumuz</strong></p><br />
+          <p className='text-orange-300'><strong>Misyonumuz</strong></p><br />
 Teknoloji ve inovasyonu birleştirerek, müşterilerimize en yüksek kalitede alüminyum ürünleri ve çözümleri sunmak. Çevreye duyarlı üretim süreçleri ve sürdürülebilirlik ilkeleri ile sektörde fark yaratmak. <br /><br />
-<p className='text-orange-500'><strong>Vizyonumuz </strong></p><br />
+<p className='text-orange-300'><strong>Vizyonumuz </strong></p><br />
 Global pazarda tanınan ve tercih edilen bir lider olarak, alüminyum sektöründe yenilikçi ve sürdürülebilir çözümler geliştirmek. <br /><br />
- <p className='text-orange-500'><strong>Değerlerimiz</strong></p><br />
+ <p className='text-orange-300'><strong>Değerlerimiz</strong></p><br />
 <strong>Kalite:</strong> Ürünlerimizin her aşamasında mükemmelliği hedefleyerek, müşteri memnuniyetini sağlamayı taahhüt ederiz. <br />
 <strong>İnovasyon:</strong> Sürekli olarak yeni teknolojiler ve yöntemler geliştirerek sektörde öncü olmayı amaçlarız. <br />
 <strong>Çevreye Duyarlılık:</strong> Üretim süreçlerimizde çevresel sürdürülebilirliği ve doğa dostu uygulamaları ön planda tutarız. <br />
